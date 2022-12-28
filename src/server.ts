40459c84239b748c37c1d1ba054e1dba9503d9d8
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2022-12-28 08:18:29 -0500

    Configurando apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import express from 'express';
+import 'reflect-metadata';
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import {BookResolver} from './resolvers/book.resolver';
+import { Server } from 'http';
 
 export async function startServer(){
     const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({resolvers:[BookResolver]})    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app,path: '/graphql'})
+
     return app;
-}
+};
